a84594c62f8dadb3986fbbc86a232f0afa09d324
turtton <watagame2579@gmail.com>
2022-03-06 14:20:43 +0900

    WIP

--- client-examples/src/main/scala/io/github/kory33/s2mctest/examples/SimpleClient_1_12_2.scala
+++ client-examples/src/main/scala/io/github/kory33/s2mctest/examples/SimpleClient_1_12_2.scala
@@ -13,13 +13,31 @@ import io.github.kory33.s2mctest.impl.client.abstraction.{
   TimeUpdateAbstraction
 }
 import io.github.kory33.s2mctest.impl.clientpool.ClientInitializationImpl
+import io.github.kory33.s2mctest.impl.connection.packets.PacketDataPrimitives.{
+  LenPrefixedSeq,
+  UByte,
+  VarInt
+}
+import io.github.kory33.s2mctest.impl.connection.packets.PacketDataCompoundTypes.Slot
+import io.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Play.ClientBound.{
+  ChunkData_withBlockEntity,
+  DeclareRecipes,
+  EntityHeadLook,
+  EntityMove_i16,
+  EntityTeleport_f64,
+  EntityVelocity,
+  JoinGame_WorldNames_IsHard,
+  TeleportPlayer_WithDismount,
+  UpdateLight_WithTrust
+}
+import io.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Play.ServerBound.ClickWindow_State
 import monocle.Lens
 import monocle.macros.GenLens
 
 @main
 def simpleClient_1_12_2(): Unit = {
   import io.github.kory33.s2mctest.impl.connection.protocol.versions
-  import versions.v1_12_2.{protocolVersion, loginProtocol, playProtocol}
+  import versions.v1_17_1.{protocolVersion, loginProtocol, playProtocol}
   import cats.implicits.given
   import cats.effect.unsafe.implicits.global
 
@@ -27,7 +45,7 @@ def simpleClient_1_12_2(): Unit = {
   object WorldView {
     val unitLens: Lens[WorldView, Unit] = Lens[WorldView, Unit](_ => ())(_ => s => s)
     val worldTimeLens: Lens[WorldView, WorldTime] = GenLens[WorldView](_.worldTime)
-    val positionLens: Lens[WorldView, PositionAndOrientation] = GenLens[WorldView](_.position)
+//    val positionLens: Lens[WorldView, PositionAndOrientation] = GenLens[WorldView](_.position)
   }
 
   val address = SocketAddress.fromString("localhost:25565").get
@@ -35,7 +53,6 @@ def simpleClient_1_12_2(): Unit = {
   val packetAbstraction = ProtocolPacketAbstraction
     .empty[IO, WorldView](playProtocol)
     .thenAbstractWithLens(KeepAliveAbstraction.forProtocol, WorldView.unitLens)
-    .thenAbstractWithLens(PlayerPositionAbstraction.forProtocol, WorldView.positionLens)
     .thenAbstractWithLens(TimeUpdateAbstraction.forProtocol, WorldView.worldTimeLens)
 
   val accountPool = AccountPool.default[IO].unsafeRunSync()
@@ -63,8 +80,20 @@ def simpleClient_1_12_2(): Unit = {
         // CAUTION: Receiving a parameter `packet` here *SIGNIFICANTLY* (about 6s to 60s) increases
         // compilation time. You may wish to ignore the parameter with
         // `PacketArrived(_)` to reduce compilation time.
-        case client.ReadLoopStepResult.PacketArrived(packet) =>
-          IO(println(s"Visible packet: $packet")) >> IO.pure(None)
+        case client.ReadLoopStepResult.PacketArrived(packet) => packet match {
+            case _: EntityVelocity | _: EntityMove_i16 | _: EntityHeadLook | _: EntityTeleport_f64 | _: JoinGame_WorldNames_IsHard | _: ChunkData_withBlockEntity | _: UpdateLight_WithTrust | _: DeclareRecipes =>
+              IO.none
+            case _: TeleportPlayer_WithDismount => IO(println("seeeeeeeeeeeeeend!!!!!!!!!!!!!!")) >> client.writePacket(ClickWindow_State[Slot.Upto_1_17_1](
+              UByte(0),
+              VarInt(0),
+              0,
+              0,
+              VarInt(0),
+              LenPrefixedSeq(Vector()),
+              Slot.Upto_1_17_1(false, None, None, None)
+            )) >> IO.pure(None)
+            case _ => IO(println(s"Visible packet: $packet")) >> IO.pure(None)
+          }
       }
     }
 
